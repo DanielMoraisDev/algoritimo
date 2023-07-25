c638feb6e335fcfe681c572a186eba20fc92af07
Daniel <danielfelipe2905@gmail.com>
2023-07-25 09:14:42 -0300

    adicionado atualizações

--- 02_ESTRUTURA_SEQUENCIAL/index_01_1.js
+++
@@ -1,5 +0,0 @@
-let variavel = "Olá"
-console.log(variavel, typeof variavel)
-
-variavel = 43
-console.log(variavel, typeof variavel)
--- 02_ESTRUTURA_SEQUENCIAL/index_01_2.js
+++
@@ -1,7 +0,0 @@
-let nome = prompt("Digite seu nome:")
-let idade = prompt("Digite sua idade:")
-
-Number.parseInt(nome)
-alert(nome, idade)
-console.log(nome, idade)
-document.write(nome, idade)
