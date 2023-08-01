0a84ac82a318f7a9d494a00cf04737621a734fc8
Daniel <danielfelipe2905@gmail.com>
2023-08-01 08:00:11 -0300

    Estrutura condional adicionado

---
+++ 01_estrutura_condicional_simples.js
@@ -0,0 +1,21 @@
+alert('Estrutura condicional simples')
+
+//ele compara apenas com o que está dentro da condição
+if(true){
+    alert('instrução dentro do if')
+
+}
+
+//&& é igual a "e" do portugol
+if(2+2==4 && 2==2){
+    alert('instrução dentro do if')
+
+}
+
+// || é igual á "ou" do portugol
+if(2+2==4 || 2==2){
+    alert('instrução dentro do if')
+
+}
+
+//OBS: O condiconal não faz operação, apenas comparação por isso ==
---
+++ 02_estrutura_condicional_composta.js
@@ -0,0 +1,14 @@
+alert('Estrutura condicional simples')
+
+//ele compara apenas com o que está dentro da condição
+if(true){
+    alert('instrução dentro do if')
+}
+
+debugger
+if(true){
+    alert('bloco de codigo 01 executado')
+}else{
+    alert('bloco de codigo 02 executado')
+}
+
