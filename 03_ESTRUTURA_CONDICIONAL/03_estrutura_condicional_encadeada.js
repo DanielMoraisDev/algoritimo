306f8b09ab00e8c508c9cc11174d74655183222f
Daniel <danielfelipe2905@gmail.com>
2023-08-01 09:45:41 -0300

    novas adicçõies ;D

--- 03_ESTRUTURA_CONDICIONAL/03_estrutura_condicional_encadeada.js
+++ 03_ESTRUTURA_CONDICIONAL/03_estrutura_condicional_encadeada.js
@@ -1,8 +1,8 @@
 let numero = 3
 
 debugger
-switch(numero){
-    case 1,2:
+switch(numero){ //OBS: como foi observador ele sempre vai executar o primeiro e foda-se o segundo kkkk
+    case 1,2,3:
         alert('comando 01')
         break
     case 3: //no caso dele não encontrar ele quebra
