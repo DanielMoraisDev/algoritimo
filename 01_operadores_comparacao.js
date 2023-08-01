c2eec021a62ae2ea7345959b068ae457cf15e5cb
Daniel <danielfelipe2905@gmail.com>
2023-08-01 10:09:17 -0300

    novas adicçõies ;D

---
+++ 01_operadores_comparacao.js
@@ -0,0 +1,10 @@
+console.log(2 == 2) // true
+console.log(2 == '2')// true 2
+console.log(2 === '2') // false
+console.log(2 != '2') //false 2
+console.log(2 !== '2') //true
+console.log(2 > '2')//false, 2 nao é maior que 2
+console.log(2 >= '2')//true, não analisa o tipo logo é true
+console.log(2 < '2')// false, pois nao é maior que 2
+console.log(2 <= '2')//true, pois nao anaisa o tipo
+<===
